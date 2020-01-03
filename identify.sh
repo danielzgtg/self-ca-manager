450d075575296d196c320a8d56996fd6cd4a09b6
Daniel Tang <danielzgtg.opensource@gmail.com>
2020-01-03 02:53:22 -0500

    Implement identify

--- identify.sh
+++ identify.sh
@@ -3,3 +3,32 @@
 . util/welcome.sh
 
 echo 'Will set your identity.'
+echo 'Please enter your info below'
+echo
+
+read -p 'Country Code (2 letters): ' -r -n 2 C
+echo
+read -p 'State or Province: ' -r ST
+read -p 'Locality (City): ' -r L
+read -p 'Name: ' -r O
+read -p 'Email Address: ' -r emailAddress
+echo
+
+RESULT='[ my_req_distinguished_name ]
+C = '"$C"'
+ST = '"$ST"'
+L = '"$L"'
+O  = '"$O"'
+CN = '"$O"'
+emailAddress = '"$emailAddress"
+
+echo 'Please confirm:'
+echo "$RESULT"
+echo
+
+. util/prompt.sh
+
+mkdir -p identity
+echo "$RESULT" > identity/identity.conf
+
+echo 'Identified!'
