98cdb51952296f891425f2ed7e97b4a5298f53c7
Daniel Tang <danielzgtg.opensource@gmail.com>
2020-01-03 20:29:38 -0500

    Add tests

---
+++ testcase.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [[ ! -f testmode ]]; then
+  echo 'You are not running tests'
+  echo 'DO NOT run this ./testcase.sh script!!!'
+  echo 'It will delete your data every time!!!'
+  exit 1
+fi
+
+err() {
+  echo 'A test failed on line '"$1"
+  exit 1
+}
+
+set -o errtrace
+trap 'err $LINENO $' ERR
+
+echo 'Automated test cases'
+echo 'This will DELETE all existing data!!!'
+
+. util/prompt.sh
+
+cleanup() {
+  rm -rf ca
+  rm -rf identity
+  rm -rf req
+}
+
+identity-setup() {
+  echo 'Identity Setup'
+  echo -n "\
+CA\
+Ontario
+Toronto
+John Doe
+test@example.com
+y" | ./identify.sh
+}
+
+ca-init() {
+  echo -n 'y' | ./ca-configure.sh
+  echo -n 'y' | ./ca-setup.sh
+}
+
+req-init() {
+  echo -n 'y' | ./req-configure.sh
+  echo -n 'y' | ./req-setup.sh
+}
+
+req-send() {
+  ./req-send.sh
+}
+
+ca-sign() {
+  printf "y\ny\n" | ./ca-sign.sh
+}
+
+ca-respond() {
+  ./ca-respond.sh
+}
+
+req-bundle() {
+  ./req-bundle.sh
+}
+
+identity-setup
+ca-init
+req-init
+req-send
+ca-sign
+ca-respond
+req-bundle
+
+echo 'Tests passed'
